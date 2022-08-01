8985c76bf84159bdae824f19152a0611928ec492
K M Rifat ul alom <kmrifat@gmail.com>
2022-08-01 22:12:14 +0600

    working on form

--- src/views/webpages/pages/JobApply.jsx
+++ src/views/webpages/pages/JobApply.jsx
@@ -1,23 +1,42 @@
 import { useParams } from "react-router-dom"
 import useJobDetails from "../../../hooks/jobdetails"
+import axios from "axios"
 
 function JobApply() {
     let { job, setJob } = useJobDetails()
 
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert('Hello')
+    }
+
     return (
         <div className="row justify-content-center mt-3">
             <div className="col-md-6">
-                <div className="card">
+                <form onSubmit={handleSubmit} className="card">
                     <div className="card-header">
                         Apply For the Position of {job.title}
                     </div>
                     <div className="card-body">
-                        Form
+                        <div className="mb-3">
+                            <label className="form-label">Email address</label>
+                            <input type="email" className="form-control" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">Full Name</label>
+                            <input type="email" className="form-control" />
+                        </div>
+
+                        <div className="mb-3">
+                            <label className="form-label">Full Name</label>
+                            <input type="email" className="form-control" />
+                        </div>
                     </div>
                     <div className="card-footer">
-                        <button className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary">Submit</button>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     )
